6a62c8bae73a268103315acd9c04a42faa5a373d
Aleksey <aleks-herou@mail.ru>
2021-02-27 18:49:37 +0300

    Проект

---
+++ SparqlTry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SparqlTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
