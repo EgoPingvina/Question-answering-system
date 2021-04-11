5e9033b67b674e55a509764d8e627b053d146454
Aleksey <aleks-herou@mail.ru>
2021-04-11 20:39:50 +0300

    Работающий пример запроса

--- SparqlTry/Program.cs
+++ SparqlTry/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Query.Optimisation;
+using VDS.RDF.Writing.Formatting;
 
 namespace SparqlTry
 {
     class Program
     {
-        static void Main(string[] args)
+		public static void Main(string[] args)
+		{
+            SparqlDBPedia();
+
+            Console.ReadKey();
+
+        }
+
+        public static void SparqlDBPedia()
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Options.HttpDebugging = true;
+
+                var query =
+                    @"
+                        PREFIX dbpedia: <http://dbpedia.org/resource/>
+                        PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+                        SELECT ?abstract
+                        WHERE {
+                            dbpedia:Junagadh dbpedia-owl:abstract ?abstract.
+                        }
+                    ";
+
+                var endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+                var results = endpoint.QueryWithResultSet(query);
+
+                using (var response = endpoint.QueryRaw(query))
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            Console.WriteLine(reader.ReadLine());
+                        }
+                        reader.Close();
+                    }
+                    response.Close();
+                }
+
+            }
+            finally
+            {
+                Options.HttpDebugging = false;
+            }
         }
     }
 }
