4d959d9561c57b2f3663598f9b80f1d25363c405
Aleksey <aleks-herou@mail.ru>
2021-04-24 12:09:20 +0300

    Подключены и настроены Swagger и DryIoc

---
+++ OntoMath-QAS/Program.Container.cs
@@ -0,0 +1,19 @@
+﻿using DryIoc;
+
+namespace OntoMath_QAS
+{
+    public partial class Program
+    {
+        private static IContainer Container { get; }
+                 = new Container().With(rules => rules.With(propertiesAndFields: PropertiesAndFields.Auto));
+
+        /// <summary>
+        /// Регистрация типов в IoC-контейнере.
+        /// </summary>
+        /// <param name="container">Используемый IoC-контейнер</param>
+        public static void Register(IContainer container)
+        {
+
+        }
+    }
+}
--- OntoMath-QAS/Program.cs
+++ OntoMath-QAS/Program.cs
@@ -4,12 +4,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
+using DryIoc;
+using DryIoc.Microsoft.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+
 namespace OntoMath_QAS
 {
-    public class Program
+    public partial class Program
     {
         public static IHostBuilder CreateHostBuilder(string[] args)
             => Host.CreateDefaultBuilder(args)
+                   .UseServiceProviderFactory(new DryIocServiceProviderFactory(Container))
+                   .UseSerilog((context, services, options) =>
+                   {
+                       options
+                           .ReadFrom.Configuration(context.Configuration)
+                           .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Debug)
+                           .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                           .MinimumLevel.Override("System", LogEventLevel.Warning)
+                           .Enrich.FromLogContext()
+#if DEBUG
+                           .WriteTo.Console(LogEventLevel.Information)
+                           .WriteTo.Debug(LogEventLevel.Information)
+#endif
+#if !SEQ
+                           .WriteTo.Seq(
+                               "http://localhost:5341",
+                               LogEventLevel.Information)
+#endif
+                           ;
+                   })
                    .ConfigureAppConfiguration((context, options) =>
                    {
                        options
@@ -20,6 +45,10 @@ namespace OntoMath_QAS
                    {
                        webBuilder.UseStartup<Startup>()
                                  .UseIIS();
+                   })
+                   .ConfigureContainer<Container>((context, container) =>
+                   {
+                       Program.Register(container);
                    });
 
         public static void Main(string[] args)
