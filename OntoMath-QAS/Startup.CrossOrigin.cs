8b3c8fd70a84b6e8e9b23088fbcc9971488ee727
Aleksey <aleks-herou@mail.ru>
2021-04-24 15:39:03 +0300

    Удалено лишнее+комментарии

--- OntoMath-QAS/Startup.CrossOrigin.cs
+++ OntoMath-QAS/Startup.CrossOrigin.cs
@@ -4,10 +4,19 @@ using Microsoft.AspNetCore.Cors.Infrastructure;
 
 namespace OntoMath_QAS
 {
+    /// <summary>
+    /// Настройка CORS-политики.
+    /// </summary>
     public sealed partial class Startup
     {
+        /// <summary>
+        /// Имя политики безопасности.
+        /// </summary>
         private readonly string AllowAnyPolicyName = "Any";
 
+        /// <summary>
+        /// Конфигурирование политики безопасности.
+        /// </summary>
         private Action<CorsPolicyBuilder> AllowAny
          => new Action<CorsPolicyBuilder>(builder =>
                 builder.AllowAnyOrigin()
